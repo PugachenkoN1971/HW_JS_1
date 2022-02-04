9e125e7733cf73369d0b889e49a9a495c2529009
PugachenkoN1971 <pugachenko.nikolay1@gmail.com>
2022-02-04 18:23:20 +0200

    sad

--- lesson1_CW/index.js
+++ lesson1_CW/index.js
@@ -1,5 +1,5 @@
 
-/* */
+/*       */
 let massiv = [34, 65, 54, [23, 86, 43, 93, 'all'], 21, 'finger', 23, 63, 'run', 86];
 console.log(massiv[0]);
 console.log(massiv[1]);
@@ -20,6 +20,7 @@ console.log(massiv[3][3]);
 
 let massiv1 = [34, 65, 54, 69, 21, 99, 23, 63, 11, 86];
 let result;
+
 console.log(massiv1[0]);
 console.log(massiv1[1]);
 console.log(massiv1[2]);
@@ -30,10 +31,24 @@ console.log(massiv1[6]);
 console.log(massiv1[7]);
 console.log(massiv1[8]);
 console.log(massiv1[9]);
-
 result=massiv1[0]+massiv1[1]+massiv1[2]+massiv1[3]+massiv1[4]+massiv1[5]+massiv1[6]+massiv1[7]+massiv1[8]+massiv1[9];
 console.log('Summa massiva:');
 console.log(result);
+
+let k;
+for(k=0; k<10; k++) {
+    massiv1[k] = Math.floor(Math.random()*100);
+}
+
+document.write(`<h2>Масив: ${massiv1[0]}  ${massiv1[1]} ${massiv1[2]} ${massiv1[3]} ${massiv1[4]} ${massiv1[5]} 
+${massiv1[6]} ${massiv1[7]} ${massiv1[8]} ${massiv1[9]}</h2>`);
+let j;
+result=0;
+for(j=0;j<10;j++) {
+    result = result +massiv1[j];
+}
+console.log(result);
+document.write(`<h2>Сумма масиву дорівнює ${result}</h2>`);
 
 /* 2. Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр */
 
@@ -86,23 +101,64 @@ let book_2 = [
         avtor: 'Steve Forbes'
     },
 ];
-
+/*  Чтобы вывести результат 4-го задания в окно браузера раскоментируйте следующую строку  */
+/*
 console.log(book_2[1].name);
 document.write('=================================');
-  for (let i=0; i<5; i++) {
-      document.write(
-          `<div>
+for (let i=0; i<5; i++) {
+    document.write(
+        `<div>
                <h2>${i+1}</h2>
                <h2>${book_2[i].name}</h2>
                <h2>${book_2[i].kolichestvo}</h2>
                <h2>${book_2[i].dganr}</h2>
-               <h2>${book_2[i].avtor}</h2>                
+               <h2>${book_2[i].avtor}</h2>
         </div>`);
-      document.write('============================');
-      }
+    document.write('============================');
+}
 /*   5. Напишіть код, який вираховує площу прямокутника висотою 23см. (змінна height)
         та шириною 10см (змінна width). Значення площі зберігати в змінній s.
  */
 
-   let height=23;
-   let width=10;
+let height;
+let width;
+let s;
+height=23;
+width=10;
+s = height * width;
+document.write(`<h2>Площа прямокутника висотою 23см та шириною 10см дорівнює ${s} см</h2>`);
+
+/* 6. Напиши код, який знаходить об'єм циліндра заввишки 10м (змінна heightC) та діаметром основи 4м (dC),
+      результат помістіть у змінну v.
+ */
+
+let heightC=10;
+let dC=4;
+let v;
+let v1;
+let v2;
+
+v1=Math.PI*Math.pow(dC/2,2)*heightC;
+v2=Math.PI*(dC/2)**2*heightC;
+//v2=9; /* Чтобы правильно считался объм целиндра, закоментируйте эту строку */
+v=v1;
+if(v1===v2) {
+    document.write(`<h2>Об'єм циліндра заввишки 10м та діаметром основи 4м дорівнює ${v} м</h2>`);
+} else {
+    document.write(`<h2>Не вдається обчислити об'єм циліндра. Щось криво працює, перевірте код, будь ласка.</h2>`);
+}
+
+/* 7. У прямокутного трикутника дві сторони n (зі значенням 3) та m (зі значенням 4).
+Знайдіть гіпотенузу k за теоремою Піфагора (потрібно використовувати функцію Math.pow (число, ступінь)
+або оператор зведення в ступінь **).
+*/
+
+let k1=3;
+let k2=4;
+let g;
+
+g=Math.pow(k1**2 + k2**2, 0.5);
+
+
+document.write(`<h2>Якщо у прямокутного трикутника сторони (катети) дорівнють 3 та 4, 
+                    то за теоремою Піфагора гіпотенуза дорівнює  ${g}</h2>`);
