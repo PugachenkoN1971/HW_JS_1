b2d5a8b6e15ea199a37d42f8badf1797b60250a8
PugachenkoN1971 <pugachenko.nikolay1@gmail.com>
2022-02-09 12:43:33 +0200

    sad

--- lesson4_CW/index.js
+++ lesson4_CW/index.js
@@ -167,6 +167,59 @@ document.write(`<h3>Минимальное число массива ${min1}</h3
 document.write(`<h2>___Задание 8____Створити функцію яка заповнює масив рандомними числами
   (цей код генерує рандомні числа в діапазоні від 0 до 100 - Math.round(Math.random()*100)) 
   та виводить його</h2>`);
-function mass11(razmerMass) {
-     //let mass
+console.log('___________Задание №8__________________');
+function massFunk(razmerMass) {
+     let i;
+     let massRand=[];
+     document.write(`<h2>Всего элементов в массиве: ${razmerMass}</h2>`)
+     for(i=0;i<razmerMass;i++)  {
+         massRand[i]=Math.round(Math.random()*100);
+         console.log(massRand[i]);
+         document.write(`<h3>Элемент [${i+1}] массива равен: ${massRand[i]} </h3>`);
+     }
+}
+massFunk(10);
+/* 9. Створити функцію яка заповнює масив рандомними числами в діапазоні               */
+/*    від 0 до limit. limit - аргумент, який характеризує кінцеве значення діапазону.  */
+document.write(`<h2>___Задание 9____Створити функцію яка заповнює масив рандомними
+ числами в діапазоні від 0 до limit. limit - аргумент, 
+ який характеризує кінцеве значення діапазону</h2>`);
+function massLim(limit) {
+    let i;
+    let massRand=[];
+    document.write(`<h2>______Mассив с максимальными значениями элементов: ${limit}________</h2>`)
+    for(i=0;i<10;i++)  {
+        massRand[i]=Math.round(Math.random()*limit);
+        console.log(massRand[i]);
+        document.write(`<h3>Элемент [${i+1}] массива равен: ${massRand[i]} </h3>`);
+    }
+}
+massLim(10);
+massLim(100);
+massLim(1000);
+/*   10. Функція приймає масив та робить з нього новий масив в зворотньому  */
+/*     порядку. [1,2,3] -> [3, 2, 1].                                       */
+/****************************************************************************/
+document.write(`<h2>___Задание 10____Функція приймає масив та робить з нього новий масив
+ в зворотньому порядку. [1,2,3] -> [3, 2, 1]</h2>`);
+let mass12=[1,2,3,4,5,6,7,8,9,10,11,12,13];
+let mass14=[];
+let k;
+function masReverse(massIn,massOut) {
+    let i;
+    for(i=0;i<massIn.length;i++) {
+        massOut[massIn.length-i-1]=massIn[i];
+    }
+}
+masReverse(mass12,mass14);
+console.log('___________________________________________');
+document.write(`<h3>________Массив с расположением элементов в прямом порядке________</h3>`);
+for(k=0;k<mass12.length;k++) {
+    console.log(mass12[k]);
+    document.write(`<h3>Элемент [${k}] массива mass12 соответственно равен: ${mass12[k]}</h3>`);
+}
+document.write(`<h3>________Массив с расположением элементов в обрантом порядке________</h3>`);
+for(k=0;k<mass14.length;k++) {
+    console.log(mass14[k]);
+    document.write(`<h3>Элемент [${k}] массива mass14 соответственно равен: ${mass14[k]}</h3>`);
 }
