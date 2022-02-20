36151f02763cc008d85348f8e5a7d63f79852558
PugachenkoN1971 <pugachenko.nikolay1@gmail.com>
2022-02-20 05:51:40 +0200

    sad

--- lesson6_CW/index.js
+++ lesson6_CW/index.js
@@ -189,8 +189,26 @@ console.log(massChis);
 sortNums('umenshenia');
 document.write(`<h3>Массив, отсортированный на убывание: ${massChis}</h3>`);
 console.log(massChis);
-
-
+/**********************************************************************
+/*    7. є масив
+/*          let coursesAndDurationArray = [
+/*                 {title: 'JavaScript Complex', monthDuration: 5},
+/*                 {title: 'Java Complex', monthDuration: 6},
+/*                 {title: 'Python Complex', monthDuration: 6},
+/*                 {title: 'QA Complex', monthDuration: 4},
+/*                 {title: 'FullStack', monthDuration: 7},
+/*                 {title: 'Frontend', monthDuration: 4}
+/*                  ];
+/*      7.1.  Відсортувати його за спаданням за monthDuration
+************************************************************************/
+let coursesAndDurationArray = [
+                 {title: 'JavaScript Complex', monthDuration: 5},
+                 {title: 'Java Complex', monthDuration: 6},
+                 {title: 'Python Complex', monthDuration: 6},
+                 {title: 'QA Complex', monthDuration: 4},
+                 {title: 'FullStack', monthDuration: 7},
+                 {title: 'Frontend', monthDuration: 4}
+                  ];
 
 
 
